89efc49d1ee851ec6785bd3e43624d834253235b
Jose Pablo Gonzalez Patarro <josep.gonzalez@eviden.com>
2024-02-13 16:51:41 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora-JPGP';
+  numero1:number= 0;
+  numero2:number= 0;
+  resultado:number= 0;
+
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numero1 * this.numero2;
+  }
+
+  dividir(): void {
+    if (this.numero2 !== 0) {
+      this.resultado = this.numero1 / this.numero2;
+    } else {
+      alert('No puedes dividir por cero.');
+    }
+  }
+
+
 }
